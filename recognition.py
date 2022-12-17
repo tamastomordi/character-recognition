4f3747c4bc5fe762b1784efcbf99a84824e362da
Tamás Tömördi <tomordi.tamas@gmail.com>
2022-12-17 22:31:48 +0100

    Change img format in recognition

--- recognition.py
+++ recognition.py
@@ -5,25 +5,25 @@ import cv2
 import matplotlib.pyplot as plt
 import numpy as np
 
-model = keras.models.load_model('./model_4')
+model = keras.models.load_model('./saved_models/model_lenet5')
 
 print("Type the name of the file: ")
 file_name = input()
 
-img = cv2.imread('./letters/' + file_name)
+image = cv2.imread('./letters/' + file_name, cv2.IMREAD_GRAYSCALE)
+image = cv2.resize(image, (28, 28), interpolation = cv2.INTER_LINEAR)
+image = cv2.bitwise_not(image)
 
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-resized = cv2.resize(gray, (28, 28), interpolation = cv2.INTER_AREA)
+img = np.array(image)
+img = img / 255
+img = img.reshape(-1, 28, 28, 1)
 
-newimg = keras.utils.normalize(resized, axis = 1)
-newimg = np.array(newimg).reshape(-1, 28, 28, 1)
-
-predictions = model.predict(newimg)
+predictions = model.predict(img)
 
 if np.argmax(predictions) < 26:
    print("This is the letter: ", chr(97 + np.argmax(predictions)))
 else:
    print("This is the digit: ", chr(48 + (np.argmax(predictions) - 26)))
 
-plt.imshow(resized, cmap = plt.cm.binary)
+plt.imshow(image, cmap='gray')
 plt.show()
